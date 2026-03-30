56d5d0d8e5604a9139042510b141ab2f00eebdae
Lucas Mew <lucas.piereti.dev@gmail.com>
2026-03-30 17:55:53 -0300

    Criando a classe passiente

---
+++ src/main/java/br/com/hospital_sao_rafael/crm/domain/Paciente.java
@@ -0,0 +1,18 @@
+package br.com.hospital_sao_rafael.crm.domain;
+
+public class Paciente {
+
+    private Long id;
+    private String nome;
+    private Double peso;
+    private Double altura;
+    private Double imc;
+
+    public void calcularIMC() {
+        if (peso != null && altura != null && altura > 0) {
+            this.imc = peso / (altura * altura);
+        }
+    }
+
+    // getters e setters
+}
