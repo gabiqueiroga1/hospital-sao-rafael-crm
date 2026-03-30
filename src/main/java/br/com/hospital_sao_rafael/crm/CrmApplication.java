3654380b9e4cc4202cb7ef0e009f29acac72cb0a
queiroga.gab <gabriela.queiroga1603@gmail.com>
2026-03-30 17:20:12 -0300

    Add files via upload

---
+++ src/main/java/br/com/hospital_sao_rafael/crm/CrmApplication.java
@@ -0,0 +1,13 @@
+package br.com.hospital_sao_rafael.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/hospital_sao_rafael/crm/CrmApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hospital_sao_rafael.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
